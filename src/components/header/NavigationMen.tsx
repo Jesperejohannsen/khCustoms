ed5877de64c53e92b9228c3b8731a6a912956354
Jesperejohannsen <jesperejohannsen@gmail.com>
2023-12-20 07:07:23 +0100

    making reusable navigationmenu

---
+++ src/components/header/NavigationMen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
+interface MenuProps {
+  items: MenuItem[];
+}
+
+const NavigationMenu: React.FC<MenuProps> = ({ items }) => (
+  <nav>
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>
+          <Link to={item.route}>{item.name}</Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
+
+export default NavigationMenu;
