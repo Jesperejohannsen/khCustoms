d7172530bd36f74ffdfa5d04c8959c8c9ea48332
Jesperejohannsen <jesperejohannsen@gmail.com>
2023-12-23 08:46:59 +0100

    adding footer mockup and some dummy css

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './footer.css';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p>KH Customs, Fredericiavej 55, 7100 Vejle Danmark</p>
+      <p>Tlf: 30 74 17 29</p>
+      <p>CVR: 44 12 96 63</p>
+    </footer>
+  );
+}
+
+export default Footer;
