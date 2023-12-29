67bf03e6bb6ea8a07e926f1c9d2c12ac218b815f
Jesperejohannsen <you@example.com>
2023-12-29 19:48:42 +0100

    adding linkedin icon

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
   return (
@@ -7,6 +9,9 @@ const Footer: React.FC = () => {
       <p>KH Customs, Fredericiavej 55, 7100 Vejle Danmark</p>
       <p>Tlf: 30 74 17 29</p>
       <p>CVR: 44 12 96 63</p>
+      <a href="https://www.linkedin.com/company/kh-customs-aps/" target="_blank" rel="noopener noreferrer">
+       <FontAwesomeIcon icon={faLinkedin} className="linkedin-icon" />
+      </a>
     </footer>
   );
 }
