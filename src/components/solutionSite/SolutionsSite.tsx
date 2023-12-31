25a28605c4b3ce9ad3280fce8bc06fdab65ad302
Jesperejohannsen <you@example.com>
2023-12-31 07:20:26 +0100

    adding css and text

--- src/components/solutionSite/SolutionsSite.tsx
+++ src/components/solutionSite/SolutionsSite.tsx
@@ -1,15 +1,41 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShip, faTruck, faPlane } from "@fortawesome/free-solid-svg-icons";
+import compliancePicture from "../../assets/compliance.png"; 
 import "./solutionsSite.css";
 
 const SolutionsSite = () => {
   return (
     <div className="solutions-site">
-      <h1>Our Solutions</h1>
-      <p>
-        We provide custom clearance services for import/export of cargo from all
-        around the world.
-      </p>
+      <div className="solutionsSectionOne">
+        <div>
+          <h1>Our Solutions</h1>
+          <p>
+            We provide custom clearance services for import/export of cargo from
+            all around the world.
+          </p>
+          <img src={compliancePicture} alt="" />
+        </div>
+        <div>
+          <p>
+            Vi kan hjælpe dig med følgende:
+            <ul>
+              <li>Toldklarering indenfor sø, luft og vej</li>
+              <li>Gennemgang af dine toldforhold og tolddokumenter</li>
+              <li>Vejledning om toldregler i EU</li>
+              <li>Assistance i forhold til varekoder</li>
+              <li>Toldoplags vejledning og regnskab</li>
+              <li>
+                Etablering af kontakt til toldstyrelsen, samt hjælpe ved
+                revision og bevillingstilsyn
+              </li>
+              <li>Hjælp til ansøgning om toldbevillinger</li>
+              <li>Vejledning om besparelser på told og afgifter</li>
+              <li>Vejledning omkring Brexit og de engelske toldregler</li>
+              <li>Rettelsesangivelser og manuelle toldopkrævninger</li>
+            </ul>
+          </p>
+        </div>
+      </div>
       <div className="solutions-details">
         <div>
           <h2>Sea Freight</h2>
